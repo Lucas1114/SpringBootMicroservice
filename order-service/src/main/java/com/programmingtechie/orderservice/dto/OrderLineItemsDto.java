289b92363b3c891b395678d190ef02e00a67b0cf
Lucas1114 <lucas.feb4@gmail.com>
2023-08-10 22:59:48 +1000

    add order service

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/dto/OrderLineItemsDto.java
@@ -0,0 +1,22 @@
+package com.programmingtechie.orderservice.dto;
+
+import com.programmingtechie.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+
+    private long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+    private List<OrderLineItems> orderLineItemsList;
+
+}
